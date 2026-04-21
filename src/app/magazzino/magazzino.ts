0f6c1f2263823431226df44960d0effd4aa6c6a2
Mattia Maddaloni <mattiamaddalo2004@gmail.com>
2026-04-21 10:06:40 +0200

    add Prodotto Magazzino Dto component

---
+++ src/app/magazzino/magazzino.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-magazzino',
+  imports: [],
+  templateUrl: './magazzino.html',
+  styleUrl: './magazzino.css',
+})
+export class Magazzino {}
---
+++ src/app/prodotto/prodotto.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prodotto',
+  imports: [],
+  templateUrl: './prodotto.html',
+  styleUrl: './prodotto.css',
+})
+export class Prodotto {}
