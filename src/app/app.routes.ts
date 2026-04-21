3e690c34610d07b02dd63f40522d5c2746a87243
Mor33mila <lorenzolomb20@gmail.com>
2026-04-21 12:01:40 +0200

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home-component/home-component';
+import { CarrelloComponent } from './carrello-component/carrello-component';
+import { CatalogoComponent } from './catalogo-component/catalogo-component';
+import { MagazzinoComponent } from './magazzino-component/magazzino-component';
+import { OrdineComponent } from './ordine-component/ordine-component';
+import { ProdottoComponent } from './prodotto-component/prodotto-component';
+import { SpedizioneComponent } from './spedizione-component/spedizione-component';
+import { UserComponent } from './user-component/user-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+     { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'carrello', component: CarrelloComponent},
+    { path: 'catalogo', component: CatalogoComponent},
+    { path: 'magazzino', component: MagazzinoComponent},
+    { path: 'ordine', component: OrdineComponent},
+    { path: 'prodotto', component: ProdottoComponent},
+    { path: 'spedizione', component: SpedizioneComponent},
+    { path: 'user', component: UserComponent},
+    { path: '**', redirectTo: 'home' }
+];
