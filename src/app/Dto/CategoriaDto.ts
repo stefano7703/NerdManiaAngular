5e287f4765d63659f085cbedbd90d788a00f3438
stefano7703 <162974312+stefano7703@users.noreply.github.com>
2026-04-21 10:05:57 +0200

    add: abstract-service, categoriaDto, UserDto, usercomponente, user-service, service

---
+++ src/app/Dto/CategoriaDto.ts
@@ -0,0 +1,18 @@
+export class CategoriaDto {
+  id: number;
+  nome: string;
+  catalogo: CatalogoDto;
+  prodotti: ProdottoDto[];
+
+  constructor(
+    id: number,
+    nome: string,
+    catalogo: CatalogoDto,
+    prodotti: ProdottoDto[]
+  ) {
+    this.id = id;
+    this.nome = nome;
+    this.catalogo = catalogo;
+    this.prodotti = prodotti;
+  }
+}
