312230f4b1ddfec3445b7d936b437958901680bf
Mor33mila <lorenzolomb20@gmail.com>
2026-04-21 10:33:05 +0200

    Add. Carrello/Catalogo DTO/COMPNT         home-component

---
+++ src/app/carrello-component/carrello-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrello-component',
+  imports: [],
+  templateUrl: './carrello-component.html',
+  styleUrl: './carrello-component.css',
+})
+export class CarrelloComponent {}
---
+++ src/app/catalogo-component/catalogo-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalogo-component',
+  imports: [],
+  templateUrl: './catalogo-component.html',
+  styleUrl: './catalogo-component.css',
+})
+export class CatalogoComponent {}
