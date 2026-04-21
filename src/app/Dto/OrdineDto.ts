a03fa982073f3a1e4c4b8ed13b2fc873657b9718
adinoolfi <giuseppe.adinolfi.01@gmail.com>
2026-04-21 10:02:22 +0200

    add: OrdineDto SpedizioneDto

---
+++ src/app/Dto/OrdineDto.ts
@@ -0,0 +1,26 @@
+export class OrdineDto {
+
+  id: number;
+
+  costo_totale: number;
+
+  numero_prodotti : number;
+
+  indirizzo_spedizione: string;
+
+  user: UserDto;
+
+  spedizione: SpedizioneDto;
+
+  prodotti: ProdottoDto[] = [];
+
+  constructor(id : number , costo_totale: number, numero_prodotti: number, indirizzo_spedizione:string, user: UserDto, spedizione: SpedizioneDto, prodotti: ProdottoDto[]) {
+    this.id = id;
+    this.costo_totale = costo_totale;
+    this.numero_prodotti = numero_prodotti;
+    this.indirizzo_spedizione=indirizzo_spedizione;
+    this.user=user;
+    this.spedizione=spedizione;
+    this.prodotti=prodotti;
+}
+}
