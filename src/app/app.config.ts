8946813e209975e582bd73b5964ed9e895ddb72e
stefano7703 <162974312+stefano7703@users.noreply.github.com>
2026-04-22 10:33:16 +0200

    add: addUserComponent, LoginComponent, authInterceptor; modify: app, UserDto, UserService, UserComponent

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,9 +4,9 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
-export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './auth.interceptor';
+
+export const appConfig = {
+  providers: [provideRouter(routes), provideHttpClient(withInterceptors([authInterceptor]))],
 };
