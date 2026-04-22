ec9a86b344e2151f8bf46fc59535cb2bcb6c1e2a
stefano7703 <162974312+stefano7703@users.noreply.github.com>
2026-04-22 15:32:38 +0200

    modify: profilo, login, registrazione e logout

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,20 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './auth.interceptor';
 
-export const appConfig = {
-  providers: [provideRouter(routes), provideHttpClient(withInterceptors([authInterceptor]))],
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+
+    provideHttpClient(
+      withInterceptors([authInterceptor])
+    ),
+
+    provideClientHydration(withEventReplay())
+  ]
 };
