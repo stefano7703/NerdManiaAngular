fcdf60e1949c4ed9d7a0bab2975c2872323cb063
Mattia Maddaloni <mattiamaddalo2004@gmail.com>
2026-04-24 11:15:42 +0200

    edit app.html app.routes.ts create admin-prodotto-component css html ts adminguard.ts

---
+++ src/app/Guard/AdminGuard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from '../Service/AuthService';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminGuard implements CanActivate {
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  
+  canActivate(): boolean {
+    if (this.authService.isLoggedIn() && this.authService.isAdmin()) {
+      return true;
+    }
+
+    this.router.navigate(['/home']);
+    return false;
+  }
+  
+}
